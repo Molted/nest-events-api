ee4c545166136a356b356e28967447705a479bee
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-09-09 21:19:00 +0800

    Custom Configuration Files and Options

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './events/entities/event.entity';
 import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Event],
-      synchronize: true,
-  }),
-  EventsModule,
-],
+    ConfigModule.forRoot({
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+    }),
+    EventsModule,
+  ],
   controllers: [],
   providers: [],
 })
