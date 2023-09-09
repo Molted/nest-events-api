3ab95f6fd00892e0327be6ba264901567d7a954c
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-09-09 21:01:58 +0800

    Application Config and Environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { EventsController } from './events/events.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './events/entities/event.entity';
 import { EventsModule } from './events/events.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'nest-events',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Event],
       synchronize: true,
   }),
