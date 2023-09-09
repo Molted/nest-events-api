88bee11d13854598fa290bc2345505b448378b30
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-09-09 21:52:19 +0800

    One To Many Relation

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Attendee } from "src/events/entities/attendee.entity";
 import { Event } from "src/events/entities/event.entity";
 
 export default registerAs(
@@ -11,7 +12,7 @@ export default registerAs(
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [Event],
+    entities: [Event, Attendee],
     synchronize: true,
   }),
 );
