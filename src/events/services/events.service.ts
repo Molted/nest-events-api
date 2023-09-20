e8702e383441da75f92580d934fef898b4e5125e
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-09-20 16:21:07 +0800

    Joins and Aggregation with Query Builder

--- src/events/services/events.service.ts
+++ src/events/services/events.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Event } from '../entities/event.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AttendeeAnswerEnum } from '../entities/attendee.entity';
 
 @Injectable()
 export class EventsService {
@@ -18,8 +19,40 @@ export class EventsService {
       .orderBy('e.id', 'DESC');
   }
 
+  public getEventsWithAttendeeCount() {
+    return this.getEventsBaseQuery()
+      .loadRelationCountAndMap('e.attendeeCount', 'e.attendees')
+      .loadRelationCountAndMap(
+        'e.attendeeAccepted',
+        'e.attendees',
+        'attendee',
+        (qb) =>
+          qb.where('attendee.answer = :answer', {
+            answer: AttendeeAnswerEnum.Accepted,
+          }),
+      )
+      .loadRelationCountAndMap(
+        'e.attendeeMaybe',
+        'e.attendees',
+        'attendee',
+        (qb) =>
+          qb.where('attendee.answer = :answer', {
+            answer: AttendeeAnswerEnum.Maybe,
+          }),
+      )
+      .loadRelationCountAndMap(
+        'e.attendeeRejected',
+        'e.attendees',
+        'attendee',
+        (qb) =>
+          qb.where('attendee.answer = :answer', {
+            answer: AttendeeAnswerEnum.Rejected,
+          }),
+      );
+  }
+
   async getEvent(id: number): Promise<Event | undefined> {
-    const query = this.getEventsBaseQuery()
+    const query = this.getEventsWithAttendeeCount()
       .andWhere('e.id = :id', { id });
 
     this.logger.debug(query.getSql());
